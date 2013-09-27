b505e8016f5a5003fdfa12619600df91e1a8ae97
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-09-27 14:16:18 +0300

    Add test repo manifest
    For testing gbp pq-rpm, for now.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- bootstrap.py
+++ bootstrap.py
@@ -34,9 +34,11 @@ from tests.component.rpm import RepoManifest
 LOG = logging.getLogger(os.path.basename(sys.argv[0]))
 TEST_PKGS = {'gbp-test-native': {'build_branches': ['master']},
              'gbp-test-native2': {'build_branches': ['master']},
-             'gbp-test': {'build_branches': ['master', 'fork']},
+             'gbp-test': {'build_branches': ['master', 'fork'],
+                          'export_branches': ['master', 'upstream']},
              'gbp-test2': {'build_branches': ['master'],
-                           'export_branches': ['master', 'upstream']}}
+                           'export_branches': ['master', 'master-orphan',
+                           'upstream']}}
 
 
 class GitError(Exception):
